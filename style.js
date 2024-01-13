d026b78b83293dee3b2f8d009ccf0932f3854e8f
likhita-maddala <maddala.likhita@gmail.com>
2024-01-13 21:13:32 +0530

    First commit

---
+++ style.js
@@ -0,0 +1,9 @@
+
+function toggleDropdown() {
+    var content = document.getElementById("dropdownContent");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
